586df7078866e77e4283774120bd46b2a4b869b9
henr0t <h.trieu@live.nl>
2020-07-09 22:25:28 +0200

    Variable Scope

--- Demo.java
+++ Demo.java
@@ -1,37 +1,31 @@
 package com.company;
 
 class Demo {
+    static int variable2;                           //class variable
+    int variable;                                   // instance variable/fields
+    String se = "big";
+
     public static void main(String[] args) {
+        //Scope of variables
+        // Local variables (also known as method-local variables)
+        // Method parameters (also known as method arguments)
+        // Instance variables (also known as attributes, fields, and nonstatic variables)
+        // Class variables (also known as static variables)
 
-        byte b = 10;
-        short s = 10;
-        int i = 10;
-        long l = 10l;
+        int variable;                               //local variable
+        int variable2;
 
-        char c = 'a';
+        Demo.variable2 =10;                         //to call class variable
 
-        l = i;              //smaller primitives fits in bigger ones
-        i = (int) l;         //to go the other way around you have to cast
+        new Demo().shadowingexample();
+    }
 
-        i = c;              //char fits in int and bigger
-        b = (byte) c;        //from short and smaller char needs to be cast
+    void methodparameters(int variable){            //method parameter
+        this.variable = 10;                         //reach field with .this (can't be done in a static class)
+    }
 
-        float f1 = 20.0F;
-        float f2 = 5.0f;
-        double d = f1 + f2;   //float auto converts to double
-
-        int i1 = 10;
-        long l1 = 40;
-        long i2 = i1 + l1;    //when long is involved in an arithmetic operator it auto widens to lon
-
-        byte b1 = 5;
-        short s1 = 5;
-        int x = b1 + s1;      //arithmetic operators with byte and short widens to int
-
-        double d1 = 50.0;
-        short s2 = 50;
-        double x1 = d1 + s2;  //widens to double
-        
-
+    void shadowingexample(){                        //shadowing is when the variable with the smallest scope is used
+        String se = "small";                        //over the bigger scope (see String se)
+        System.out.println(se);
     }
 }
