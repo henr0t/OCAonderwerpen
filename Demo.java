8b818713ed4c80c9a720e9c68eab4d14f31dc787
henr0t <h.trieu@live.nl>
2020-07-12 21:24:37 +0200

    Flow control

--- Demo.java
+++ Demo.java
@@ -1,61 +1,62 @@
 package com.company;
 
 class Demo {
-    public static void main(String[]args) {
-        //Wrapper classes
-        // wrapper classes are used to wrap primitives in an object, so they can be added to a collection of object.
-        //wrapper classes are immutable-classes
+    public static void main(String[] args) {
+        int x = 0;
+        if (x == 0) System.out.println("zonder {} 1e statement geldig");
+        else if (x == 1) {
+            System.out.println("else if");
+        } else {
+            System.out.println("else statement");
+        }
 
-        //Numeric wrapper classes:
-        int whatever = 12;
+        String a = x == 0 ? "true" : "false";
+        System.out.println("Korte if notitie: " + a);
 
-        Byte byte1 = 10;                //This is the autoboxing method for wrapper classes
-        Short short1 = 10;              //
-        Integer int1 = whatever;        //
-        Long long1 = 108456798L;        //
-        Float float1 = 10.12345f;       //
-        Double double1 = 10.98;         //
+        int y = 0;
+        switch (y) {
+            default:
+                System.out.println("default can be anywhere.");
+            case 1:
+                System.out.println("without break it keeps going");
+            case 2:
+                System.out.println("See!!!");
+                break;
+            case 4:
+            case 3:
+            case 5:
+                System.out.println("doesn't print");
+        }
 
-        //auto unboxing
-        long long2 = long1;
+        //for loops
+        for (int i = 0; i < 3; i++) {
+            System.out.println("repeats " + i + " times.");
+        }
 
-        //Object wrapper classes
-        Boolean bool1 = true;           //
-        Character char1 = 'a';          //
+        //enhanced for loops
+        String[] strArray = new String[2];
+        for (String element : strArray) {
+            System.out.println("enhanced loops: " + element);
+        }
 
-        //Constructor method
-        Boolean bool2 = new Boolean(true);
-        Boolean bool3 = new Boolean("true");
+        int z = 0;
+        while (z < 2) {
+            System.out.println("while loops: " + z);
+            z++;
+        }
 
-        //static method valueOf()
-        Boolean bool4 = Boolean.valueOf(true);
-        Boolean bool5 = Boolean.valueOf("TrUE");
+        int w = 0;
+        do {
+            System.out.println("do-while loops: " + w);
+            w++;
+        } while (w != 2);
 
-        System.out.println(">>>>"+(bool4 == bool5));
-
-        //None of these classes define a no-argument constructor.
-        //wrapper classes are immutable, so it doesn't make sense to initialize with the default primitive value
-        //all wrapper classes (except character) define a constructor that accepts a String argument
-        //constructors always create new instances,
-        // valueOf returns a cached copy for applicable value, same goes for autoboxing
-
-        //(exceptions based on the cached range of said value)
-//        Cached instances exist for the wrapper Boolean class for the values
-//        true and false. The Character class caches instances with values from 0 to
-//        127. Classes Byte, Short, Integer, and Long cache instances for values -127 to
-//        128. No cached instances exist for the Float and Double wrapper classes.
-
-        Integer int3 = Integer.valueOf(200);
-        Integer int4 = Integer.valueOf(200);
-
-        System.out.println("== method: "+(int3 == int4));
-        System.out.println("equals method: "+int3.equals(int4));
-        //.equals  compares the primitive value stored by a wrapper
-        //checking whether they refer to the same instance
-
-        //objects of different wrapper classes with the same values are not equal
-        //equals() will return false, == won't compile
-
-
+        LABEL1:
+        for (int i = 0; i < 5; i++) {
+            if (i == 2) {
+                break LABEL1;
+            }
+            System.out.println("Labels: "+i);
+        }
     }
 }
