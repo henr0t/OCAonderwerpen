52324529b1c0b522fb0a965fc757fa556b7b80d9
henr0t <h.trieu@live.nl>
2020-09-22 11:46:32 +0200

    accepted switch arguments

--- Demo.java
+++ Demo.java
@@ -1,47 +1,31 @@
 package com.company;
 
-import com.company.other.*;
-
-class Demo extends Demo2{
+class Demo{
 
     public static void main(String[] args) {
-        new A().whatever(50);
+        byte varbyte = 0;
+        char varchar = 0;
+        short varshort = 0;
+        int varint = 0;
+        String string = "0";
+        //enums
 
+        //Not accepted by switch
+        //  double vardouble = 0.0;
+        //  long varlong = 0;
+        //  float varfloat = 0.0f;
+
+
+        switch (varint){
+            case 0:
+                System.out.println("prints!");
+                break;
+            default:
+                System.out.println("prints default");
+                break;
+
+        }
     }
 }
 
-class A{
-    public String toString(){
-        return "I am A, call me mr.this";
-    }
-    int x = 10;
-    static int y = 20;
-    final int z = 60;
-    public void whatever(int z){
-        int x = 30;
-        int y = 40;
-        System.out.println(this.x);
-        System.out.println(x);
-        System.out.println(A.y);
-        System.out.println(y);
-        System.out.println(this.z);
-        System.out.println(z);
-
-        System.out.println(this);
-    }
-
-    A(){
-    this(3.0);
-        System.out.println("constructor  2");
-    }
-
-    A(int x){
-        super();
-        System.out.println("constructor "+x);
-    }
-
-    A(double z){
-        this(1);
-        System.out.println("constructor "+z);
-    }
-}
+enum Numbers {ONE,TWO,THREE}
--- other/Demo2.java
+++ other/Demo2.java
@@ -1,7 +1,7 @@
 package com.company.other;
 
 public class Demo2{
-    public void demo2 (){
+    public static void demo2(){
         System.out.println("I am from package other, class Demo2");
     }
 
